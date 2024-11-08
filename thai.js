7a60f916c0b829d5d94cb84c33c125fb0e3cfaf4
snapnut <141468166+snapnut@users.noreply.github.com>
2024-11-08 18:11:19 +0700

    Rizz up thai.js

--- thai.js
+++ thai.js
@@ -3,10 +3,24 @@ function thai()
     return navigator.languages[0].substring(0,2).toLowerCase()=="th" || navigator.languages[1].substring(0,2).toLowerCase()=="th";
 }
 
-if (!thai())
+function nega()
 {
-    document.body.innerHTML="";
-    document.documentElement.remove();
-    document.querySelectorAll().remove();
-    document.write("This site isn't accessible by you.");
+    if (!thai())
+        {
+            document.body.innerHTML="";
+            document.documentElement.remove();
+            document.querySelectorAll().remove();
+            document.write("This site isn't accessible by you.");
+        }
 }
+
+window.onfocus=nega
+window.onload=nega
+
+function raf(n)
+{
+    nega()
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
